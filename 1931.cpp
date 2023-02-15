363fbef4bbdc9604ec17a6605cabf447acef6515
LeeJoonHee <neo81389@gmail.com>
2023-02-16 01:59:58 +0900

    solved 1931

--- 1931.cpp
+++ 1931.cpp
@@ -1,14 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-int main() {   
+int main() {
 
-   int n;
-   cin >> n
+   vector< vector<int> > list(1, vector<int>());
+   int n, m = 214748364-1, M = 0;
+   cin >> n;
 
-   vector< vector <int> > list(100000, vector<int>() );
+   for (int i = 0; i < n; i++) {
+      int a, b;
+      cin >> a >> b;
+      m = m > a ? a : m;
+      M  = M < b ? b : M;
 
+      if (M >= list.size()) {
+         list.resize(M+1);
+      }
+
+      list[a].__emplace_back(b);
+   }
+   vector<int> possiblity(M + 1, 0);
+   int largest = 0;
+
+   for (int i = m; i <= M; i++) {
+
+      sort(list[i].begin(), list[i].end());
+
+      largest = largest < possiblity[i] ? possiblity[i] : largest;
+
+      for (int j = 0; j < list[i].size(); j++) {
+         possiblity[list[i][j]] = largest + 1;
+         largest = largest < possiblity[i] ? possiblity[i] : largest;
+      }
+   }
+
+   cout << largest << endl;
 
    return 0;
 }
